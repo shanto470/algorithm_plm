fa41c7068d3c7397c4c3d3d299e6f19de31869c8
shanto470 <shantopaul470@gmail.com>
2025-11-25 16:40:35 +0600

    Update bicolor.cpp

--- BFS/Bicolor/bicolor.cpp
+++ BFS/Bicolor/bicolor.cpp
@@ -1,1 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while (true) {
+        int n;
+        cin >> n;
+        if (n == 0) break;
+
+        int l;
+        cin >> l;
+
+        vector<vector<int>> graph(n);
+        for (int i = 0; i < l; i++) {
+            int a, b;
+            cin >> a >> b;
+            graph[a].push_back(b);
+            graph[b].push_back(a);
+        }
+
+        vector<int> color(n, -1);
+        queue<int> q;
+
+        color[0] = 0;
+        q.push(0);
+        bool isBipartite = true;
+
+        while (!q.empty() && isBipartite) {
+            int u = q.front();
+            q.pop();
+
+            for (int v : graph[u]) {
+                if (color[v] == -1) {
+                    color[v] = 1 - color[u];
+                    q.push(v);
+                } else if (color[v] == color[u]) {
+                    isBipartite = false;
+                    break;
+                }
+            }
+        }
+
+        if (isBipartite)
+            cout << "BICOLORABLE.\n";
+        else
+            cout << "NOT BICOLORABLE.\n";
+    }
+
+    return 0;
+}
